a635c4a3c7332502eacf225287a9770ff10fa187
Ashleigh <ashleighc207@gmail.com>
2019-09-13 21:39:28 -0400

    add in second page content

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ $('.nav-link').on('click', function(e){
   $('.nav-link').removeClass('active');
   $(e.target).addClass('active');
 })
+
+if($('.header-slant')) {
+  $('body').addClass('white-text-variant')
+}
