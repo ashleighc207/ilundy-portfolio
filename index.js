cda66a3c5e9d2df8edeca8bf8e675cda2834c6e0
Ashleigh <ashleighc207@gmail.com>
2019-09-20 23:13:33 -0400

    add in mobile styles and mobile header on scroll

--- index.js
+++ index.js
@@ -3,6 +3,36 @@ $('.nav-link').on('click', function(e){
   $(e.target).addClass('active');
 })
 
-// if($('.header-slant')) {
-//   $('body').addClass('white-text-variant')
-// }
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('.mobile-header').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+
+    if (st > lastScrollTop && st > navbarHeight){
+        $('.mobile-header').removeClass('nav-down').addClass('nav-up');
+    } else {
+        if(st + $(window).height() < $(document).height()) {
+            $('.mobile-header').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+
+    lastScrollTop = st;
+}
