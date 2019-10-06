f44f8fffbd39dd027759607a0efac465a1fcd096
Ashleigh <ashleighc207@gmail.com>
2019-10-05 21:37:59 -0400

    adjust logos page, remove underlines from sticky header

--- index.js
+++ index.js
@@ -1,50 +1,59 @@
-$('.nav-link').on('click', function(e){
-  $('.nav-link').removeClass('active');
-  $(e.target).addClass('active');
-})
+$(".nav-link").on("click", function(e) {
+  $(".nav-link").removeClass("active");
+  $(e.target).addClass("active");
+});
 
 // Hide Header on on scroll down - taken from the internet, modified to support two navs
 var didScroll;
 var lastScrollTop = 0;
 var delta = 5;
-var navbarHeight = $('.sticky-header').outerHeight();
+var navbarHeight = $(".sticky-header").outerHeight();
 
-$(window).scroll(function(event){
-    didScroll = true;
+$(window).scroll(function(event) {
+  didScroll = true;
 });
 
 setInterval(function() {
-    if (didScroll) {
-        hasScrolled();
-        didScroll = false;
-    }
+  if (didScroll) {
+    hasScrolled();
+    didScroll = false;
+  }
 }, 250);
 
 function hasScrolled() {
-    var st = $(this).scrollTop();
+  var st = $(this).scrollTop();
 
-    if(Math.abs(lastScrollTop - st) <= delta)
-        return;
+  if (Math.abs(lastScrollTop - st) <= delta) return;
 
-    if(st < 156){
-      $('.sticky-header').removeClass('nav-down').addClass('nav-up').addClass('hide');
-    } else if (st > lastScrollTop && st > navbarHeight){
-        $('.sticky-header').removeClass('nav-down').addClass('nav-up').addClass('hide');
-    } else {
-        if(st + $(window).height() < $(document).height()) {
-            $('.sticky-header').removeClass('nav-up').addClass('nav-down').removeClass('hide').addClass('transition');
-        }
+  if (st < 156) {
+    $(".sticky-header")
+      .removeClass("nav-down")
+      .addClass("nav-up")
+      .addClass("hide");
+  } else if (st > lastScrollTop && st > navbarHeight) {
+    $(".sticky-header")
+      .removeClass("nav-down")
+      .addClass("nav-up")
+      .addClass("hide");
+  } else {
+    if (st + $(window).height() < $(document).height()) {
+      $(".sticky-header")
+        .removeClass("nav-up")
+        .addClass("nav-down")
+        .removeClass("hide")
+        .addClass("transition");
     }
+  }
 
-    lastScrollTop = st;
+  lastScrollTop = st;
 }
 
-$("a").click(function(e){
-    e.preventDefault();
-    if (this.href) {
-        var target = this.href;
-        setTimeout(function(){
-            window.location = target;
-        }, 750);
-    }
+$("a").click(function(e) {
+  e.preventDefault();
+  if (this.href) {
+    var target = this.href;
+    setTimeout(function() {
+      window.location = target;
+    }, 750);
+  }
 });
